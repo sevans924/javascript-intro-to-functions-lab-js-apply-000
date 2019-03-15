b5c69fb22b5242abd3e7dfe0dcbb1448a8a2b5ff
Sarah Elzinga <sevans924@gmail.com>
2019-03-15 01:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (string.toLowerCase === string){
   return 'I can\'t hear you!'
 } else if (string.toUpperCase === string){
   return 'YES INDEED!'
-} else if(string === "I love you, Grandma."){
-  return "I love you, too."
+} else if(string === 'I love you, Grandma.'){
+  return 'I love you, too.'
 }
 }
